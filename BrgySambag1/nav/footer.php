b2f7a12794e3f45205f4222bacc697df87ebef47
LinTGofficial <124423960+LinTGofficial@users.noreply.github.com>
2023-11-20 19:44:22 +0800

    Add files via upload

---
+++ BrgySambag1/nav/footer.php
@@ -0,0 +1,15 @@
+<footer>
+        <img src='drawable/Logo.png' alt='img'  class='home-logo'>
+        <div class="footer-extra-cont">
+            <div class="footer-extra">
+                <a href="about.php">Barangay Information</a>
+                <a href="officials.php">Barangay Officials</a>
+                <a href="officials.php">Barangay Committees</a>
+            </div>
+            <div class="footer-extra">
+                <a href="docreq.php">Request Document</a>
+                <a href="documentDtb.php">Document List</a>
+                <a href="news.php">News & Announcements</a>
+            </div>
+        </div>
+</footer>
